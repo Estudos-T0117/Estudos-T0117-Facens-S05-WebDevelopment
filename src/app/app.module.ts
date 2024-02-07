07811021e138e2fa790fce40dc273d1669d6af6b
Tales Augusto <tales.a.s.furlan@gmail.com>
2024-02-06 21:59:26 -0300

    feat(all): My first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuSuperiorComponent,
+    RodapeComponent,
+    ListaClienteComponent
   ],
   imports: [
     BrowserModule,
